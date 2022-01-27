5f42dc7c7167abcf76ce0b96d235e0f8732193f7
tarlan-1907 <81584399+tarlan-1907@users.noreply.github.com>
2022-01-27 10:11:08 +0400

    Add files via upload

---
+++ wsgi.py
@@ -0,0 +1,10 @@
+<<<<<<< HEAD
+from app.main import app
+
+if __name__ == "__main__":
+=======
+from app.main import app
+
+if __name__ == "__main__":
+>>>>>>> a23f336dbb2b34d939c7eea80f727e6a95246351
+  app.run()
