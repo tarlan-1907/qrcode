f7cc8ff5079dfc020f839a9d103b7175648efe76
tarlan-1907 <81584399+tarlan-1907@users.noreply.github.com>
2022-01-27 10:19:40 +0400

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 from app.main import app
 
 if __name__ == "__main__":
-=======
-from app.main import app
-
-if __name__ == "__main__":
->>>>>>> a23f336dbb2b34d939c7eea80f727e6a95246351
   app.run()
